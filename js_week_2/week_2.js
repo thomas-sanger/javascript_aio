1a4abaef99de337a23e147f8823eed9f30f0efd1
Thomas Sanger <thomas.sanger92@gmail.com>
2025-01-28 08:22:32 -0700

    Periodic update.

---
+++ js_week_2/week_2.js
@@ -0,0 +1,15 @@
+// let phoneNumber; // variable declaration
+// phoneNumber = "503-555-555"; // variable initialization
+
+// let favortieColor = "red"; // variable declaration and initialization
+
+// simple arithmetic
+
+// let seven = 7;
+// let eight = 8;
+// let sum = seven + eight; // addition that equals 15
+
+let firstNumber = prompt("Pick a number"); // prompt to pick firstNumber
+let secondNumber = prompt("Pick another number"); // prompt to pick secondNumber
+let sum = firstNumber + secondNumber; // varible sum stores addition of firstNumber and secondNumber
+alert(sum); // displays and alert with the sum
