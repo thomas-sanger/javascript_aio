b57977c9e279344b937a6b5844d6da70cbadd9c0
Thomas Sanger <thomas.sanger92@gmail.com>
2025-01-28 11:13:41 -0700

    Periodic commit.

--- js_week_2/week_2.js
+++ js_week_2/week_2.js
@@ -1,3 +1,5 @@
+// variable declartion and initialization
+
 // let phoneNumber; // variable declaration
 // phoneNumber = "503-555-555"; // variable initialization
 
@@ -9,7 +11,15 @@
 // let eight = 8;
 // let sum = seven + eight; // addition that equals 15
 
-let firstNumber = prompt("Pick a number"); // prompt to pick firstNumber
-let secondNumber = prompt("Pick another number"); // prompt to pick secondNumber
-let sum = firstNumber + secondNumber; // varible sum stores addition of firstNumber and secondNumber
-alert(sum); // displays and alert with the sum
+// using prompts to get a number and sum it
+
+// let firstNumber = prompt("Pick a number"); // prompt to pick firstNumber
+// let secondNumber = prompt("Pick another number"); // prompt to pick secondNumber
+// let sum = firstNumber + secondNumber; // varible sum stores addition of firstNumber and secondNumber
+// alert(sum); // displays and alert with the sum
+
+// constants
+
+// const LIKE_TACOS = true; // constant using the UPPER_SNAKE_CASE naming convention
+
+// data types
